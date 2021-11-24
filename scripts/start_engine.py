4143543a88c2f9f0f055c527de0c37cceeaf781c
Petro <ptiurin@gmail.com>
2021-11-24 13:34:01 +0000

    Adding stopped engine setup

--- scripts/start_engine.py
+++ scripts/start_engine.py
@@ -14,4 +14,10 @@ if __name__ == "__main__":
     engine = rm.engines.create(engine_name, scale=1, spec="m5d.4xlarge")
     engine.attach_to_database(database, True)
     engine.start()
+
+    # No start needed, stopped engine should always be stopped
+    stopped_engine_name = engine_name + "_stopped"
+    stopped_engine = rm.engines.create(stopped_engine_name, scale=1, spec="B1")
+    stopped_engine.attach_to_database(database, True)
+
     print(engine.name, engine.endpoint)
