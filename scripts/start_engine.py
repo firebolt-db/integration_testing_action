c99c3423c130504a144953890e950077690438fd
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-11-25 12:47:48 +0200

    revert instance type rename

--- scripts/start_engine.py
+++ scripts/start_engine.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     database_name = argv[1]
     engine_name = database_name
     database = rm.databases.get_by_name(database_name)
-    engine = rm.engines.create(engine_name, scale=1, spec="B4")
+    engine = rm.engines.create(engine_name, scale=1, spec="m5d.4xlarge")
     engine.attach_to_database(database, True)
     engine.start()
     print(engine.name, engine.endpoint)
