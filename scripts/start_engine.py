d8d1370ee611525b00dc7e0d73f7b7abbc5f4c7b
Petro <ptiurin@gmail.com>
2021-11-26 09:26:43 +0000

    FIR-9475: Using the cheapest instance

--- scripts/start_engine.py
+++ scripts/start_engine.py
@@ -2,6 +2,7 @@ from sys import argv
 
 from firebolt.common.settings import Settings
 from firebolt.service.manager import ResourceManager
+from firebolt.service.instance_type import HasStorage
 
 if __name__ == "__main__":
     rm = ResourceManager(Settings())
@@ -11,13 +12,14 @@ if __name__ == "__main__":
     database_name = argv[1]
     engine_name = database_name
     database = rm.databases.get_by_name(database_name)
-    engine = rm.engines.create(engine_name, scale=1, spec="m5d.4xlarge")
+    instance_spec = rm.instance_types.get_cheapest(filters=[HasStorage])
+    engine = rm.engines.create(engine_name, scale=1, spec=instance_spec)
     engine.attach_to_database(database, True)
     engine.start()
 
     # No start needed, stopped engine should always be stopped
     stopped_engine_name = engine_name + "_stopped"
-    stopped_engine = rm.engines.create(stopped_engine_name, scale=1, spec="m5d.4xlarge")
+    stopped_engine = rm.engines.create(stopped_engine_name, scale=1, spec=instance_spec)
     stopped_engine.attach_to_database(database, True)
 
     print(engine.name, engine.endpoint, stopped_engine.name, stopped_engine.endpoint)
