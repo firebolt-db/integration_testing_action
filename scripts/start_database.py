2e11c092c8fccea89fe567766848790ac42cb2ce
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-12-01 19:02:49 +0200

    fix suffix

--- scripts/start_database.py
+++ scripts/start_database.py
@@ -7,6 +7,7 @@ from firebolt.service.manager import ResourceManager
 if __name__ == "__main__":
     rm = ResourceManager(Settings())
     suffix = argv[0] if len(argv) > 0 else ""
+    suffix = suffix.replace(".\\/-", "")
     database_name = f"integration_testing_{suffix}_{int(time())}"
     rm.databases.create(database_name)
     print(database_name)
