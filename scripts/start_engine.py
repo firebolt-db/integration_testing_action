e021951c51ed85e553dd95d217d411d812354213
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2022-02-08 12:32:50 +0000

    fix: Stopped engine should not be default (#6)

--- scripts/start_engine.py
+++ scripts/start_engine.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
     # No start needed, stopped engine should always be stopped
     stopped_engine_name = engine_name + "_stopped"
     stopped_engine = rm.engines.create(stopped_engine_name, scale=1, spec=instance_spec)
-    stopped_engine.attach_to_database(database, True)
+    stopped_engine.attach_to_database(database, False)
 
     print(engine.name, engine.endpoint, stopped_engine.name, stopped_engine.endpoint)
