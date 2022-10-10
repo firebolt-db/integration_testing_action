46d2c81ff30f4f79dabbbf146eb8b6b21c9ce9c3
ptiurin <petro.tiurin@firebolt.io>
2022-10-10 16:16:34 +0100

    Fix credential passing

--- scripts/start_database.py
+++ scripts/start_database.py
@@ -1,11 +1,14 @@
-from time import time
+from os import environ
 from sys import argv
+from time import time
 
+from firebolt.client.auth import UsernamePassword
 from firebolt.common.settings import Settings
 from firebolt.service.manager import ResourceManager
 
 if __name__ == "__main__":
-    rm = ResourceManager(Settings())
+    rm = ResourceManager(Settings(auth=UsernamePassword(
+        environ["FIREBOLT_USER"], environ["FIREBOLT_PASSWORD"])))
     suffix = argv[1] if len(argv) >= 2 else ""
     suffix = suffix.replace(".", "").replace("-", "")
     database_name = f"integration_testing_{suffix}_{int(time())}"
