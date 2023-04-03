eba19eebf5be855b93f9f60124a5c57889f191c9
ptiurin <petro.tiurin@firebolt.io>
2023-04-03 09:55:35 +0100

    build: Using released version of SDK

--- setup.js
+++ setup.js
@@ -32,7 +32,7 @@ function setup_virtualenv(on_success, on_error) {
 }
 
 function install_python_dependencies(python_dir, on_success, on_error) {
-  exec(path.join(python_dir, "pip") + " install git+https://github.com/firebolt-db/firebolt-python-sdk@main retry",
+  exec(path.join(python_dir, "pip") + " install firebolt-sdk>=0.16.0 retry",
     function (error, stdout, stderr) {
       error == null ? on_success(python_dir) : on_error(error.message);
     }
