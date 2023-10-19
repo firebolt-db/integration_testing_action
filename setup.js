5177031cdd1d66733e2727c3e7ba40e0d75b8372
RotemFB <rotem.bitner@firebolt.io>
2023-10-19 16:53:27 +0300

    retry import

--- setup.js
+++ setup.js
@@ -1,9 +1,7 @@
-const core = require('@actions/core');
-const sdk = require('firebolt-sdk');
-//import * as core from '@actions/core';
-//import { Firebolt } from 'firebolt-sdk';
+import * as core from '@actions/core';
+import { Firebolt } from 'firebolt-sdk';
 
-const firebolt = sdk.Firebolt({apiEndpoint: core.getInput('api-endpoint')});
+const firebolt = Firebolt({apiEndpoint: core.getInput('api-endpoint')});
 const connection = await firebolt.connect({
   auth: {
     client_id: core.getInput('firebolt-client-id'),
