ab4fdcad208daad74a0cfb286fdf0fcbf2dd6af8
ptiurin <petro.tiurin@firebolt.io>
2024-01-24 15:28:01 +0000

    debug output

--- scripts/start_database.py
+++ scripts/start_database.py
@@ -7,6 +7,7 @@ from firebolt.common.settings import Settings
 from firebolt.service.manager import ResourceManager
 
 if __name__ == "__main__":
+    print("++++PYTHON HERE" + environ["FIREBOLT_PASSWORD"])
     rm = ResourceManager(Settings(auth=UsernamePassword(
         environ["FIREBOLT_USER"], environ["FIREBOLT_PASSWORD"]), user=None, password=None))
     suffix = argv[1] if len(argv) >= 2 else ""
