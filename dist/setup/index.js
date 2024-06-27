9adb3c09b9dd82a60ff1df4710b70f48bd894994
ptiurin <petro.tiurin@firebolt.io>
2024-06-27 11:17:02 +0100

    feat: Retry on rate limit after a backoff

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -15385,6 +15385,8 @@ __nccwpck_require__.r(__webpack_exports__);
 const maxRetries = 3;
 // Delay between retries in milliseconds
 const backoffDelayMs = 1000; // 1 second
+// If we hit rate limit, wait for 60 seconds before retrying
+const backoffDelayRateLimit = 60000; // 60 seconds
 
 
 async function retryWithBackoff(fn) {
@@ -15397,6 +15399,10 @@ async function retryWithBackoff(fn) {
                 retryCount++;
                 console.log(`Received error 502. Retrying (${retryCount}/${maxRetries})...`);
                 await new Promise(resolve => setTimeout(resolve, backoffDelayMs));
+            } else if (error.statusCode === 429) {
+                retryCount++;
+                console.log(`Received error 429. Retrying (${retryCount}/${maxRetries})...`);
+                await new Promise(resolve => setTimeout(resolve, backoffDelayRateLimit));
             } else {
                 throw error;
             }
--- util.js
+++ util.js
@@ -1,6 +1,8 @@
 const maxRetries = 3;
 // Delay between retries in milliseconds
 const backoffDelayMs = 1000; // 1 second
+// If we hit rate limit, wait for 60 seconds before retrying
+const backoffDelayRateLimit = 60000; // 60 seconds
 
 
 export async function retryWithBackoff(fn) {
@@ -13,6 +15,10 @@ export async function retryWithBackoff(fn) {
                 retryCount++;
                 console.log(`Received error 502. Retrying (${retryCount}/${maxRetries})...`);
                 await new Promise(resolve => setTimeout(resolve, backoffDelayMs));
+            } else if (error.statusCode === 429) {
+                retryCount++;
+                console.log(`Received error 429. Retrying (${retryCount}/${maxRetries})...`);
+                await new Promise(resolve => setTimeout(resolve, backoffDelayRateLimit));
             } else {
                 throw error;
             }
